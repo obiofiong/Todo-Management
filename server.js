64a26b08f6513e75d399821475c6206cea142e02
obiofiong <johnobiofiong1@gmail.com>
2021-11-13 00:12:52 +0100

    added server file

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const app = express();
+const tasks = require("./routes/tasks");
+const connectDB = require("./db/connect");
+require("dotenv").config();
+const notFound = require("./middleware/notFound");
+const errorHandlerMiddleware = require("./middleware/error");
+
+// middleware
+app.use(express.static("./public"));
+app.use(errorHandlerMiddleware);
+app.use(express.json());
+
+// routes
+app.use("/api/v1/tasks", tasks);
+app.use(notFound);
+
+const start = async () => {
+	try {
+		await connectDB(process.env.MONGO_URI);
+		app.listen("3300", () => {
+			console.log("Todo Management is running on port 3300");
+		});
+	} catch (err) {
+		console.log(err);
+	}
+};
+
+start();
