93dd7392a27169281a17993dd4146e47488d17bd
Obiofiong <johnobiofiong1@gmail.com>
2022-04-26 13:03:02 +0100

    changed port number

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const connectDB = require("./db/connect");
 require("dotenv").config();
 const notFound = require("./middleware/notFound");
 const errorHandlerMiddleware = require("./middleware/error");
+const PORT = process.env.PORT || "3300";
 
 // middleware
 app.use(express.static("./public"));
@@ -16,14 +17,14 @@ app.use("/api/v1/tasks", tasks);
 app.use(notFound);
 
 const start = async () => {
-	try {
-		await connectDB(process.env.MONGO_URI);
-		app.listen("3300", () => {
-			console.log("Todo Management is running on port 3300");
-		});
-	} catch (err) {
-		console.log(err);
-	}
+  try {
+    await connectDB(process.env.MONGO_URI);
+    app.listen(PORT, () => {
+      console.log("Todo Management is running on port 3300");
+    });
+  } catch (err) {
+    console.log(err);
+  }
 };
 
 start();
